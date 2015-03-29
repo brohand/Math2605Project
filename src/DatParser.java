04f8a0af9568523dd03be63e18f9fb442a6ea3eb
Patrick Tam <pjztam@gatech.edu>
2015-03-29 19:03:02 -0400

    created class which parses .dat files to matrices, and vice versa

--- src/DatParser.java
+++ src/DatParser.java
@@ -1,6 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -11,14 +11,67 @@ import java.util.Scanner;
  */
 public class DatParser {
 
+    /**
+     * Converts a file to a Matrix
+     *
+     * @param a path to file (relative to project root)
+     * @return a Matrix
+     * @throws FileNotFoundException
+     */
     public static Matrix datToMatrix(File a) throws FileNotFoundException{
-        Scanner file = new Scanner(new FileReader(a));
-        return null;
-
-
+        List lines = new ArrayList();
+        Scanner file = new Scanner(a);
+        while (file.hasNextLine()) {
+            lines.add(file.nextLine());
+        }
+        double[][] m = new double[lines.size()][];
+        for(int i = 0; i < m.length; i++) {
+            String line = (String)lines.get(i);
+            Scanner lineScanner = new Scanner(line);
+            List<Double> columns = new ArrayList<>();
+            while(lineScanner.hasNextDouble()) {
+                columns.add(lineScanner.nextDouble());
+            }
+            double[] row = new double[columns.size()];
+            for (int j = 0; j < row.length; j++) {
+                row[j] = columns.get(j);
+            }
+            m[i] = row;
+        }
+        return new Matrix(m);
     }
 
-    public static File matrixToDat(Matrix a) {
-        return null;
+    /**
+     * Converts a Matrix to a file
+     *
+     * @param a Matrix
+     * @param filename path to file (relative to project root)
+     * @throws FileNotFoundException
+     * @throws UnsupportedEncodingException
+     */
+    public static void matrixToDat(Matrix a, String filename) throws FileNotFoundException, UnsupportedEncodingException {
+        matrixToDat(a, filename, "UTF-8");
+    }
+
+    /**
+     * Converts a Matrix to a file
+     *
+     * @param a Matrix
+     * @param filename path to file (relative to project root)
+     * @param encoding encoding of file
+     * @throws FileNotFoundException
+     * @throws UnsupportedEncodingException
+     */
+    private static void matrixToDat(Matrix a, String filename, String encoding) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter file = new PrintWriter(filename, encoding);
+        double[][] m = a.getArray();
+        for (int i = 0; i < m.length; i++) {
+            String line = "";
+            for (int j = 0; j < m[0].length; j++) {
+                line += m[i][j] + " ";
+            }
+            file.println(line);
+        }
+        file.close();
     }
 }
